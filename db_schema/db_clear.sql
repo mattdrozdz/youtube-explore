c578e246d3e4ef72101577aa7e8c3130e46acedc
Mateusz Drożdż <mateusz212@gmail.com>
2015-11-02 21:26:34 +0100

    clear db script

---
+++ db_schema/db_clear.sql
@@ -0,0 +1,7 @@
+SET FOREIGN_KEY_CHECKS = 0;
+truncate table channel;
+truncate table channel_statistics;
+truncate table video;
+truncate table video_statistics;
+truncate table comment;
+SET FOREIGN_KEY_CHECKS = 1;
